d2872ad51e116e2cbf6213956a6b3e7190366a04
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-09 10:38:03 +0530

    finding maximum of three float values

--- generics/FindMaxValue.java
+++ generics/FindMaxValue.java
@@ -21,8 +21,17 @@ public class FindMaxValue {
 	            max = a3;
 	        return max;
 	    }
+	 public static Float maxOfFloat(Float f1,Float f2,Float f3) {
+		 Float max=f1;
+		 if(f2.compareTo(max)>0)
+			 max=f2;
+		 if(f3.compareTo(max)>0)
+			 max=f3;
+		 return max;
+	 }
 	 public static void main(String[] args) {
-		 System.out.println("welcome to the maximum of three integer");
+		System.out.println("welcome to the maximum of three integer");
 		System.out.println("the maximum value of three integer is:"+maxOfInteger(4,5,9));
+		System.out.println("the maximum of three float values is:"+maxOfFloat(5.2f,8.2f,6.3f));
 	}
 }
